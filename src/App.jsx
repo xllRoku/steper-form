72d46a54aebebb18b7c008e0fe4e27d9614d80ec
xllRoku <jhinlmlz@gmail.com>
2023-01-29 21:48:45 +0100

    personal info component done

--- src/App.jsx
+++ src/App.jsx
@@ -24,7 +24,7 @@ const Step = ({ step }) => {
 
 	return (
 		<div className='flex gap-4 mx-auto w-[200px] items-center'>
-			<span className='w-9 h-9 border-[1px] border-white  rounded-full flex items-center justify-center font-bold'>
+			<span className='w-9 h-9 border-[1px] border-white  rounded-full flex items-center justify-center font-bold text-white'>
 				{stepNumber}
 			</span>
 			<div>
@@ -37,16 +37,61 @@ const Step = ({ step }) => {
 	);
 };
 
+const InputText = ({ label, name }) => {
+	return (
+		<label className='flex flex-col'>
+			<div className='flex justify-between capitalize text-marine-blue  text-sm'>
+				<span className='font-medium'>{label}</span>
+			</div>
+			<input
+				type='text'
+				className='p-2 rounded-lg border-[1px] hover:border-purplish-blue focus:border-purplish-blue'
+				name={name}
+			/>
+		</label>
+	);
+};
+const PersonalInfo = () => {
+	return (
+		<div className='pt-8 px-20'>
+			<header>
+				<h2 className='font-bold text-marine-blue text-4xl'>
+					Personal info
+				</h2>
+				<p className='text-cool-gray'>
+					Please provide your name, email address, and phone number.
+				</p>
+			</header>
+			<form className=' mt-10'>
+				<div className='space-y-6'>
+					<div>
+						<InputText label='name' />
+					</div>
+					<div>
+						<InputText label='email address' />
+					</div>
+					<div>
+						<InputText label='phone number' />
+					</div>
+				</div>
+			</form>
+		</div>
+	);
+};
+
 function App() {
 	return (
 		<main className='h-screen flex justify-center items-center'>
-			<div className='relative '>
-				<img src={sideBar} alt='' />
-				<div className='w-full absolute top-0 space-y-6 mt-8'>
-					{STEPS.map(step => (
-						<Step step={step} />
-					))}
+			<div className='flex  bg-white p-4  rounded-md'>
+				<div className='relative '>
+					<img src={sideBar} alt='' />
+					<div className='w-full absolute top-0 space-y-6 mt-8'>
+						{STEPS.map(step => (
+							<Step step={step} />
+						))}
+					</div>
 				</div>
+				<PersonalInfo />
 			</div>
 		</main>
 	);
