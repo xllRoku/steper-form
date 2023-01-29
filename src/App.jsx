a5cb906d40c3221884471ea99ffe8ea6b4917254
xllRoku <jhinlmlz@gmail.com>
2023-01-29 22:05:11 +0100

    select plan done

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,9 @@
 import sideBar from './assets/images/bg-sidebar-desktop.svg';
 
+import arcade from './assets/images/icon-arcade.svg';
+import advanced from './assets/images/icon-advanced.svg';
+import iconPro from './assets/images/icon-pro.svg';
+
 const STEPS = [
 	{
 		stepNumber: '1',
@@ -53,7 +57,7 @@ const InputText = ({ label, name }) => {
 };
 const PersonalInfo = () => {
 	return (
-		<div className='pt-8 px-20'>
+		<>
 			<header>
 				<h2 className='font-bold text-marine-blue text-4xl'>
 					Personal info
@@ -75,7 +79,85 @@ const PersonalInfo = () => {
 					</div>
 				</div>
 			</form>
-		</div>
+		</>
+	);
+};
+
+const PLANS = [
+	{
+		title: 'arcade',
+		prices: [
+			{ annuality: 'monthly', price: 9 },
+			{ annuality: 'yearly', price: 90 }
+		],
+		image: arcade
+	},
+	{
+		title: 'advanced',
+		prices: [
+			{ annuality: 'monthly', price: 12 },
+			{ annuality: 'yearly', price: 120 }
+		],
+		image: advanced
+	},
+	{
+		title: 'pro',
+		prices: [
+			{ annuality: 'monthly', price: 15 },
+			{ annuality: 'yearly', price: 150 }
+		],
+		image: iconPro
+	}
+];
+
+const Plan = ({ plan }) => {
+	const price = plan.prices[0].price;
+	const { title, image } = plan;
+	return (
+		<button
+			className='w-40 h-44  focus:border-purplish-blue focus:bg-light-blue hover:border-purplish-blue border-[1px]  border-gray-400 p-4 rounded-md flex flex-col justify-between'
+			name='plan'
+		>
+			<img src={image} alt='' />
+			<div className='flex flex-col items-start'>
+				<p className='font-bold text-marine-blue capitalize'>{title}</p>
+				<span> ${price}/mo </span>
+			</div>
+		</button>
+	);
+};
+
+const Switch = () => {
+	return <div>hello world two</div>;
+};
+
+const SelectPlan = () => {
+	return (
+		<>
+			<h2 className='font-bold text-marine-blue text-4xl'>
+				Select your plan
+			</h2>
+			<p className='text-cool-gray'>
+				You have the option of monthly or yearly billing
+			</p>
+			<form className='flex flex-col  gap-6 mt-6'>
+				<div className='flex gap-6'>
+					{PLANS.map(plan => (
+						<Plan plan={plan} />
+					))}
+				</div>
+				<div className='flex gap-6 justify-center items-center bg-magnolia p-4 rounded-md'>
+					<p className='text-marine-blue font-bold capitalize'>
+						monthly
+					</p>
+					//TODO switch
+					<Switch />
+					<p className='text-marine-blue font-bold capitalize'>
+						yearly
+					</p>
+				</div>
+			</form>
+		</>
 	);
 };
 
@@ -91,7 +173,9 @@ function App() {
 						))}
 					</div>
 				</div>
-				<PersonalInfo />
+				<div className='pt-8 px-20'>
+					<PersonalInfo />
+				</div>
 			</div>
 		</main>
 	);
